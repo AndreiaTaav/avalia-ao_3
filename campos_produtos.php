6a7d716902cf21b7434b78764784a9034b78b752
AndreiaTaav <andreia.tavares@outlook.com>
2014-01-13 17:28:12 -0200

    Create campos_produtos.php

---
+++ campos_produtos.php
@@ -0,0 +1,27 @@
+<label>Nome do Produto:</label>
+<input type = "text" name = "nomeProduto" />
+<br/>
+<input type="radio" name="cor" value="Vermelho" checked="checked" />
+<label>Vermelho</label>
+<br/>
+<input type="radio" name="cor" value="Amarelo" />
+<label>Amarelo</label>
+<br/>
+<input type="radio" name="cor" value="Rosa" />
+<label>Rosa</label>
+<br/><br/>
+<input type="checkbox" name="produtoNovo" value="sim" />
+<label>Produto Novo</label>
+<br/><br/>
+<label>Prazo de Entrega</label>
+<select name="prazoEntrega">
+    <option>5 dias</option>
+    <option>10 dias</option>
+    <option>15 dias</option>
+    
+</select>
+</br><br/>
+<label>Detalhe do Produto : </label>
+<br/>
+<textarea name="detalheProduto"></textarea>
+<br/>
