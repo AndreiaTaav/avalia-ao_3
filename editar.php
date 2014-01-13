ef79cedaf1ea52f20b64f195219e0430d4979d15
AndreiaTaav <andreia.tavares@outlook.com>
2014-01-13 17:28:41 -0200

    Create editar.php

---
+++ editar.php
@@ -0,0 +1,41 @@
+<?php
+    require_once("menu.php");
+    
+    session_start();
+    
+    if(!isset($_SESSION["cadastros"])) {
+        
+        echo "Nao existem produtos para editar.";
+     }
+        else{
+            $id = $_REQUEST["id"];
+            $nomeProduto = $_REQUEST["nomeProduto"];
+            $produtoNovo = $_REQUEST["produtoNovo"];
+            $cor = $_REQUEST["cor"];
+            $prazoEntrega = $_REQUEST["prazoEntrega"];
+            $detalheProduto = $_REQUEST["detalheProduto"];
+           
+        
+
+            
+            $livroNovo = false;
+    if(isset($_REQUEST["livroNovo"])) {
+        $livroNovo = true;
+    }
+            
+           $produto = array();
+           $produto["nomeProduto"] = $nomeProduto;
+           $produto["produtoNovo"] = $produtoNovo;
+           $produto["cor"] = $cor;
+           $produto["prazoEntrega"] = $prazoEntrega;
+           $produto["detalheProduto"] = $detalheProduto;
+    
+            $cadastros =& $_SESSION["cadastros"];
+            $cadastros[$id] = $produto;
+        echo "Ediçao realizada com exito!";
+
+        
+        }
+    
+
+?>
